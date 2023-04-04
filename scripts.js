11fbcaa56b6af532e3b57f08fe157c5e685666ac
cagatay-usta <cagatay_usta@windowslive.com>
2023-04-04 14:22:37 +0300

    add basic functions

--- scripts.js
+++ scripts.js
@@ -1,12 +1,28 @@
-function getRndInt (min, max) {
+function getRndInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function getComputerChoice () {
-    let choice = getRndInt(1, 3);
+function getComputerChoice() {
+    let number = getRndInt(1, 3);
+    switch(number) {
+        case 1:
+            choice = "rock"
+            break;
+        case 2:
+            choice = "paper";
+            break;
+        case 3:
+            choice = "scissors";
+            break;
+    }
     return choice;
 }
 
-const computerSelection = getComputerChoice();
+function getPlayerChoice() {
 
-console.log(computerSelection);
+}
+
+const computerSelection = getComputerChoice();
+const playerSelection = getPlayerChoice();
+
+console.log(computerSelection)
